d637986b28be46d0e5960272047c106701db344f
Carlos Vieira <cadu.vieira2276@gmail.com>
2020-08-07 20:16:53 -0300

    Projeto mostrando os atributos de um pedido por meio de um enum

---
+++ Projeto_Pedido/Entities/Order.cs
@@ -0,0 +1,20 @@
+﻿using Projeto_Pedido.Entities.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace Projeto_Pedido.Entities
+{
+    class Order
+    {
+        public int Id { get; set; }
+        public DateTime Moment { get; set; }
+        public OrderStatus Status { get; set; }
+
+        public override string ToString()
+        {
+            return Id + ", " + Moment + ", " + Status;
+        }
+    }
+}
---
+++ Projeto_Pedido/Program.cs
@@ -0,0 +1,28 @@
+﻿using Projeto_Pedido.Entities;
+using System;
+using Projeto_Pedido.Entities.Enums;
+
+
+namespace Projeto_Pedido
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order = new Order
+            {
+                Id = 1080,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment
+            };
+
+            Console.WriteLine(order);
+
+            string txt = OrderStatus.PendingPayment.ToString();
+
+            OrderStatus os = Enum.Parse<OrderStatus>("Delivered");
+            Console.WriteLine(txt);
+            Console.WriteLine(os);
+        }
+    }
+}
